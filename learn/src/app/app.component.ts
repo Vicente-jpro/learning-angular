11e90e2fddba128bdd8bcbee1e45fa50e3568e46
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2025-11-09 11:28:47 +0000

    feat: component criated.

--- learn/src/app/app.component.ts
+++ learn/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'learn';
+
+  data = {
+    name: 'Vicente asfd',
+    course: 'Angular'
+  }
+
+  showAlert() {
+    alert('Button clicked!');
+  }
 }
