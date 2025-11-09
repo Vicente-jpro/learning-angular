ec5166f22c676f1e8b2f56642535880b3c5dd896
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2025-11-09 00:22:43 +0000

    feat: fist commit

---
+++ learn-api/src/main/java/com/example/learn_api/LearnApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnApiApplication.class, args);
+	}
+
+}
