e99116a0d332fbef94cc3fca567fef4715cd6ba7
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2025-11-23 10:36:32 +0100

    feat: course added.

--- learn/src/app/app.component.ts
+++ learn/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule, NgForm } from '@angular/forms';
-import { CourseCardComponent } from './course-card/course-card.component';
 import { COURSES } from '../../db-data';
 import { RoomsComponent } from './rooms/rooms.component';
 
@@ -10,22 +9,12 @@ import { RoomsComponent } from './rooms/rooms.component';
   imports: [
     RouterOutlet, 
     FormsModule, 
-    CourseCardComponent,
     RoomsComponent
   ],
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
 
-  course1 = COURSES[0];
-  
-  course2 = COURSES[1];
-  
-  course3 = COURSES[2];
 
-  showAlert() {
-    alert('Button clicked!');
-  }
 }
--- learn/src/app/rooms/rooms.component.ts
+++ learn/src/app/rooms/rooms.component.ts
@@ -5,26 +5,11 @@ import { Component, Input, OnInit } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './rooms.component.html',
-  styleUrl: './rooms.component.css'
 })
 export class RoomsComponent implements OnInit{
-
-   hotelName: string = "Vicente Hotel"
-
-  numberOfRooms: number = 34
-
-  hideRooms = false
-  hideRoomsActionName = "Hide Rooms"
 
   ngOnInit(): void {
     
   }
-
-  changeRoomVisibility(){
-    this.hideRooms = !this.hideRooms
-    this.hideRoomsActionName = this.hideRooms ? "Hide Rooms" : "Show Rooms"  
-    console.log(this.hideRooms)
-  }
-
 
 }
