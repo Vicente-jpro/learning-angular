292fd2e8fa601e93d26e242e111ee6c33b9d706e
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2025-12-10 22:33:58 +0100

    feat: Manager added.

--- learn/src/app/app.routes.ts
+++ learn/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ManagerComponent } from './manager/manager.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { HomeComponent } from './home/home.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { loginGuard } from './guards/login.guard';
 
 export const routes: Routes = [
     {
@@ -21,11 +22,25 @@ export const routes: Routes = [
                 loadComponent: () => import('./manager/edit/edit.component').then(c => c.EditComponent)
             },
             
-    ]
+        ],
+        canActivate: [loginGuard]
     },
-    { path: 'employee', component: EmployeeComponent },
+    { 
+        path: 'employee', 
+        component: EmployeeComponent,
+        canActivate: [loginGuard]
+    },
+    { 
+        path: 'login', 
+        loadComponent: () => import('./login/login.component').then(c => c.LoginComponent) 
+    },
+    
     { path: '', component: HomeComponent },
-    { path: '**', component: NotfoundComponent}
+    { 
+        path: '**', 
+        loadComponent: () => import('./notfound/notfound.component').then(c => c.NotfoundComponent)
+    },
+
    // {path: '**', redirectTo: '/home', pathMatch: 'full'} 
 ];
 
--- learn/src/app/login/login.component.ts
+++ learn/src/app/login/login.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
 
